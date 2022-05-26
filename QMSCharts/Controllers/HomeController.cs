01b3aaaadfa8dcf1c194097b960d7acf77a085ca
leetrent <10787590+leetrent@users.noreply.github.com>
2022-05-26 15:26:14 -0500

    Total by status dev (#1)
    * Created a controller and model class for TotalByStatusDev
    
    * Added TotalsByStatus page which is currently emtpy
    
    * Got rid of null string warnings
    
    * Prepping TotalsByStatus.cshtml page
    
    * Pruning
    
    * Pruning
    
    * Pruning
    
    * Pruning
    
    * Implemented pie chart for TotalByStatus
    
    * Refining TotalsByStatus.cshtml
    
    * Fixed formatting issue
    
    * Added percentage to pie chart
    
    * Finished TOtalbyStatus pie chart

--- QMSCharts/Controllers/HomeController.cs
+++ QMSCharts/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace QMSCharts.Controllers
             return View();
         }
 
+        public IActionResult TotalsByStatus()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ QMSCharts/Controllers/QMSDashboardController.cs
@@ -0,0 +1,62 @@
+﻿using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using QMSCharts.Models;
+using System.Globalization;
+
+namespace QMSCharts.Controllers
+{
+    public class QMSDashboardController : Controller
+    {
+        public ActionResult TotalsByStatus_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetTotalsByStatus());
+        }
+
+        private IEnumerable<TotalByStatus> GetTotalsByStatus()
+        {
+            var totalsByStatus = new TotalByStatus[]
+            {
+                new TotalByStatus(1, "Assigned", 79),
+                new TotalByStatus(2, "Closed - Action Completed", 1916),
+                new TotalByStatus(3, "Closed - No Action Required", 234),
+                new TotalByStatus(4, "Draft", 10),
+                new TotalByStatus(5, "Pending Review", 11),
+                new TotalByStatus(6, "Returned", 92),
+                new TotalByStatus(7, "Unassigned", 5),
+            };
+
+            int grandTotal = 0;
+            foreach (var tbs in totalsByStatus)
+            {
+                grandTotal += tbs.Total;
+            }
+
+            string logSnippet = "\n[QMSDashboardController][GetTotalsByStatus] =>";
+            //Console.WriteLine($"\n{logSnippet} (grandTotal: {grandTotal})");
+
+            foreach (var tbs in totalsByStatus)
+            {
+                tbs.Percentage = (double)tbs.Total / (double)grandTotal;
+                //string percentageAsString = tbs.Percentage.ToString("P1", CultureInfo.InvariantCulture);
+                string percentageAsString = tbs.Percentage.ToString("P1", CultureInfo.CreateSpecificCulture("en-US"));
+
+                tbs.StatusAndPercentage = $"{tbs.Status} ({percentageAsString})";
+
+
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine($"{logSnippet} (tbs.Total)..............: {tbs.Total})");
+                Console.WriteLine($"{logSnippet} (grandTotal).............: {grandTotal})");
+                Console.WriteLine($"{logSnippet} (tbs.Percentage).........: {tbs.Percentage})");
+                Console.WriteLine($"{logSnippet} (percentageAsString).....: {percentageAsString})");
+                Console.WriteLine($"{logSnippet} (tbs.StatusAndPercentage): {tbs.StatusAndPercentage})");
+                Console.WriteLine("---------------------------------------------------------");
+
+                //Console.WriteLine($"\n{logSnippet} (tbs.Percentage: {tbs.Percentage})");
+                //Console.WriteLine($"\n{logSnippet} (tbs.Percentage: {tbs.Percentage})");
+                //Console.WriteLine($"\n{logSnippet} (tbs.Percentage: {tbs.Percentage.ToString("P1", CultureInfo.InvariantCulture)})");
+            }
+
+            return totalsByStatus;
+        }
+    }
+}
