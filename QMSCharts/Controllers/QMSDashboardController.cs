1076011e61e76f45a441f7fcae0cad8da376896f
LeeTrent <lee.trent.1@gmail.com>
2022-05-26 12:38:54 -0500

    Created a controller and model class for TotalByStatusDev

---
+++ QMSCharts/Controllers/QMSDashboardController.cs
@@ -0,0 +1,43 @@
+﻿using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using QMSCharts.Models;
+
+namespace QMSCharts.Controllers
+{
+    public class QMSDashboardController : Controller
+    {
+        public ActionResult TotalsByStatus_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetTotalsByStatus());
+        }
+
+        private IEnumerable<TotalByStatus> GetTotalsByStatus()
+        {
+            var totalsByStatus = new TotalByStatus[]
+            {
+                new TotalByStatus(1, "Assigned", 79),
+                new TotalByStatus(2, "Closed - Action Completed", 1916),
+                new TotalByStatus(3, "Closed - No Action Required", 234),
+                new TotalByStatus(4, "Draft", 10),
+                new TotalByStatus(5, "Pending Review", 11),
+                new TotalByStatus(6, "Returned", 92),
+                new TotalByStatus(7, "Unassigned", 5),
+            };
+
+            int grandTotal = 0;
+            foreach (var tbs in totalsByStatus)
+            {
+                grandTotal += tbs.Total;
+            }
+
+            Console.WriteLine("\n[QMSDashboardController][GetTotalsByStatus] => (grandTotal: %d)", grandTotal);
+
+            foreach (var tbs in totalsByStatus)
+            {
+                tbs.Percentage = tbs.Total / grandTotal;
+            }
+
+            return totalsByStatus;
+        }
+    }
+}
---
+++ QMSCharts/Models/TotalByStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace QMSCharts.Models
+{
+    public class TotalByStatus
+    {
+        public TotalByStatus(int id, string status, int total)
+        {
+            Id = id;
+            Status = status;
+            Total = total;
+        }
+
+        public int Id { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public int Total { get; set; }
+        public double Percentage { get; set; }
+    }
+}
