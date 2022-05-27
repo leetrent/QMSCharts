b564027212b06c6d8d9d8711457b92a147130115
leetrent <10787590+leetrent@users.noreply.github.com>
2022-05-27 17:24:09 -0500

    Total by organization dev (#3)
    * Just getting started
    
    * Completed Totals by Organization charts

---
+++ QMSCharts/Models/QMSChart.cs
@@ -0,0 +1,16 @@
+﻿namespace QMSCharts.Models
+{
+    public class QMSChart
+    {
+        public QMSChart(int id, string key, int value)
+        {
+            Id = id;
+            Key = key;
+            Value = value;
+        }
+
+        public int Id { get; set; }
+        public string Key { get; set; } = string.Empty;
+        public int Value { get; set; }
+    }
+}
